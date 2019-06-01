e671060119aec6a9bee552bc25cc2f5c3cd6c658
Etienne Gauthier <gaue3012@usherbrooke.ca>
2019-05-31 20:59:15 -0400

    added hexagon class

--- src/application/EMRApplication.java
+++ src/application/EMRApplication.java
@@ -2,9 +2,12 @@ package application;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+import models.Hexagon;
 
 public class EMRApplication extends Application {
 
@@ -17,8 +20,14 @@ public class EMRApplication extends Application {
 	// Override the start() method.
 	public void start(Stage primaryStage) {
 		try {
-			Parent root = FXMLLoader.load(getClass().getResource("/views/EMR_ui.fxml"));
+			Pane root = FXMLLoader.load(getClass().getResource("/views/EMR_ui.fxml"));
 			Scene scene = new Scene(root,600,600);
+			
+			Hexagon shape1 = new Hexagon();
+			Node hexagon = shape1.createHexagon();
+			
+			root.getChildren().add(hexagon);
+			
 			primaryStage.setScene(scene);
 			primaryStage.setTitle("EMR Application");
 			primaryStage.show();
